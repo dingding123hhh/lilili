cf7ee78e5db8d6ccbd47b519b725007e091c89a5
dingding123hhh <168184738+dingding123hhh@users.noreply.github.com>
2024-07-05 21:26:38 +0800

    Add files via upload

--- vb白名单系统.lua
+++ vb白名单系统.lua
@@ -2,7 +2,7 @@
 local whitu = false
 local Name = game.Players.LocalPlayer.Name
 
-if Name == "xszxd" then 
+if Name == "xszxwhbdd" then 
 whitu = true 
 elseif Name == "123fa98" then 
 whitu = true 
@@ -10,12 +10,106 @@ elseif Name == "Roblox_OldSix666" then
 whitu = true 
 elseif Name == "shdhudgwu" then 
 whitu = true 
-elseif Name == "cn_nc" then 
+elseif Name == "ch_pop" then 
+whitu = true 
+elseif Name == "HanMoRanl" then 
+whitu = true 
+elseif Name == "HQ6668" then 
+whitu = true 
+elseif Name == "yyds88670" then 
+whitu = true 
+elseif Name == "Horizon11003" then 
+whitu = true 
+elseif Name == "kaikainb1" then 
+whitu = true 
+elseif Name == "VX_yyrcxg" then 
+whitu = true 
+elseif Name == "GUCCA" then 
+whitu = true 
+elseif Name == "China_91666" then 
+whitu = true 
+elseif Name == "123456widhjd" then 
+whitu = true 
+elseif Name == "qwsdfghjk24" then 
+whitu = true 
+elseif Name == "HQ666789" then 
+whitu = true 
+elseif Name == "chdydshjc" then 
+whitu = true 
+elseif Name == "chinakebi" then 
+whitu = true 
+elseif Name == "xxysz7" then 
+whitu = true 
+elseif Name == "dhtufgur" then 
+whitu = true 
+elseif Name == "XM_HHM" then 
+whitu = true 
+elseif Name == "nanchuan123" then 
+whitu = true 
+elseif Name == "litingxian2009" then 
+whitu = true 
+elseif Name == "WuCuanRx" then 
+whitu = true 
+elseif Name == "BART" then 
+whitu = true 
+elseif Name == "songmaso" then 
+whitu = true 
+elseif Name == "xiaoqpalzm" then 
+whitu = true 
+elseif Name == "1q1q6y6y" then 
+whitu = true 
+elseif Name == "choukabuwaizhenjun" then 
+whitu = true 
+elseif Name == "qazplm258044" then 
+whitu = true 
+elseif Name == "ZR_MZR" then 
+whitu = true 
+elseif Name == "S207676" then 
+whitu = true 
+elseif Name == "yyc201013" then 
+whitu = true 
+elseif Name == "cmhehebeb" then 
+whitu = true 
+elseif Name == "2867qiqu" then 
+whitu = true 
+elseif Name == "zhabgzhizhen" then 
+whitu = true 
+elseif Name == "ikun4268" then 
+whitu = true 
+elseif Name == "yukjm2" then 
+whitu = true 
+elseif Name == "sonice_464" then 
+whitu = true 
+elseif Name == "Steamer_terraria" then 
+whitu = true 
+elseif Name == "SQVVnxeg" then 
+whitu = true 
+elseif Name == "wsygchxbm" then 
+whitu = true 
+elseif Name == "541888jojo" then 
+whitu = true 
+elseif Name == "phvvjji" then 
+whitu = true 
+elseif Name == "ycy899666" then 
+whitu = true 
+elseif Name == "xiao_noobA" then 
+whitu = true 
+elseif Name == "Ucdnavg" then 
+whitu = true 
+elseif Name == "Simpsons367" then 
+whitu = true 
+elseif Name == "kr520l" then 
+whitu = true 
+elseif Name == "emere2628" then 
+whitu = true 
+elseif Name == "ch_k40" then 
+whitu = true 
+elseif Name == "HanK15251456" then 
 whitu = true 
 end
 
 if whitu then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/fgg/main/VB.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/renlua/Roblox/main/Script_Hub.lua"))()
 else
     game.Players.LocalPlayer:Kick("你没有被加入白名单")
 end
